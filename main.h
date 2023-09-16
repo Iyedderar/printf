2ecc2c20186f60a8721a2e6ddb1e8ddb5276cbd9
Iyedderar <Iyedderar@yahoo.com>
2023-09-16 23:43:04 +0100

    first task : Handling %, c, s

--- main.h
+++ main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <limits.h>
 
 int _printf(const char *format, ...);
 #endif
