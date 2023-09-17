9167d725e79f99a44209d79eeeb69a53702c6c8e
BenterkiaAyoub <benterkia.ayoub1@gmail.com>
2023-09-17 14:16:59 -0700

    second edit for task two

--- functions.c
+++ functions.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * printf_char - prints a char.
- * @val: arguments.
+ * printf_char - Prints a char.
+ * @val: Arguments.
  * Return: 1.
  */
 int printf_char(va_list val)
@@ -15,9 +15,9 @@ int printf_char(va_list val)
 }
 
 /**
- * printf_string - print a string.
- * @val: argument.
- * Return: the length of the string.
+ * printf_string - Print a string.
+ * @val: Argument.
+ * Return: The length of the string.
  */
 int printf_string(va_list val)
 {
@@ -40,51 +40,5 @@ int printf_string(va_list val)
 			_putchar(s[i]);
 		return (len);
 	}
-}
-
-/**
- * printf_integer - print an integer.
- * @val: argument.
- * Return: the number of digits printed.
- */
-int printf_integer(va_list val)
-{
-	int n = va_arg(val, int);
-	int len = 0;
-	int is_negative = 0;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		is_negative = 1;
-		n = -n;
-		len++;
-	}
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-
-	len += print_digits(n);
-	return (len);
-}
-
-/**
- * print_digits - helper function to print digits.
- * @n: integer to print.
- * Return: the number of digits printed.
- */
-int print_digits(int n)
-{
-	int len = 0;
-
-	if (n == 0)
-		return (0);
-
-	len += print_digits(n / 10);
-	_putchar((n % 10) + '0');
-	return (len + 1);
 }
 
