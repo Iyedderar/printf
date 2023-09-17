83608b95664f465283e51107bb6467f3883d9eee
Iyedderar <Iyedderar@yahoo.com>
2023-09-17 14:54:37 +0100

    first task : Handling %, c, s

--- main.h
+++ main.h
@@ -7,13 +7,18 @@
 #include <stdio.h>
 #include <limits.h>
 
-
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
 typedef struct format
 {
 	char *id;
 	int (*f)();
 } convert_match;
-
+int _strlen(char *s);
 int printf_37(void);
 int printf_char(va_list val);
 int printf_string(va_list val);
